74dee3187f71d787c63c6dabbab2a7aa5b3d7470
hungtrd <hungtranduy.dev@gmail.com>
2025-04-18 15:22:59 +0700

    update test

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,6 +25,7 @@ return {
         "golangci-lint-langserver",
         "templ",
         "iferr",
+        "delve",
         "go-debug-adapter",
         -- Proto
         "buf-language-server",
@@ -99,11 +100,6 @@ return {
         tailwindcss = {
           filetype = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "templ" },
           init_options = { userLanguages = { templ = "html" } },
-        },
-        --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
-        --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
-        tsserver = {
-          enabled = false,
         },
         ts_ls = {
           enabled = false,
@@ -234,61 +230,7 @@ return {
           -- disable tsserver
           return true
         end,
-        vtsls = function(_, opts)
-          -- LazyVim.lsp.on_attach(function(client, buffer)
-          --   client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
-          --     ---@type string, string, lsp.Range
-          --     local action, uri, range = unpack(command.arguments)
-          --
-          --     local function move(newf)
-          --       client.request("workspace/executeCommand", {
-          --         command = command.command,
-          --         arguments = { action, uri, range, newf },
-          --       })
-          --     end
-          --
-          --     local fname = vim.uri_to_fname(uri)
-          --     client.request("workspace/executeCommand", {
-          --       command = "typescript.tsserverRequest",
-          --       arguments = {
-          --         "getMoveToRefactoringFileSuggestions",
-          --         {
-          --           file = fname,
-          --           startLine = range.start.line + 1,
-          --           startOffset = range.start.character + 1,
-          --           endLine = range["end"].line + 1,
-          --           endOffset = range["end"].character + 1,
-          --         },
-          --       },
-          --     }, function(_, result)
-          --       ---@type string[]
-          --       local files = result.body.files
-          --       table.insert(files, 1, "Enter new path...")
-          --       vim.ui.select(files, {
-          --         prompt = "Select move destination:",
-          --         format_item = function(f)
-          --           return vim.fn.fnamemodify(f, ":~:.")
-          --         end,
-          --       }, function(f)
-          --         if f and f:find("^Enter new path") then
-          --           vim.ui.input({
-          --             prompt = "Enter move destination:",
-          --             default = vim.fn.fnamemodify(fname, ":h") .. "/",
-          --             completion = "file",
-          --           }, function(newf)
-          --             return newf and move(newf)
-          --           end)
-          --         elseif f then
-          --           move(f)
-          --         end
-          --       end)
-          --     end)
-          --   end
-          -- end, "vtsls")
-          -- -- copy typescript settings to javascript
-          -- opts.settings.javascript =
-          --   vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
-        end,
+        vtsls = function(_, opts) end,
       },
     },
   },
---
+++ lua/plugins/test.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "fredrikaverpil/neotest-golang",
+  },
+}
