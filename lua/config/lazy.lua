d9481407572e8f8624ce95cfe9addf959919342a
hungtrd <hungtranduy.dev@gmail.com>
2026-02-11 14:41:24 +0700

    refactor lsp config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,12 +17,6 @@ require("lazy").setup({
         colorscheme = "nordfox",
       },
     },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- { import = "lazyvim.plugins.extras.dap" },
-    -- { import = "lazyvim.plugins.extras.lang.java" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,14 +10,11 @@ return {
         "lua-language-server",
         "shellcheck",
         "shfmt",
-        -- Frontend
+        -- Frontend (tailwind, html, css - không có extra)
         "tailwindcss-language-server",
-        "typescript-language-server",
         "css-lsp",
-        "markdownlint",
         "html-lsp",
-        -- "htmx-lsp",
-        -- Golang
+        -- Golang (không dùng lang.go extra)
         "gopls",
         "gofumpt",
         "goimports",
@@ -29,14 +26,8 @@ return {
         "go-debug-adapter",
         -- proto
         "buf-language-server",
-        -- markdown
-        "prettier",
-        "markdownlint-cli2",
-        "markdown-toc",
         -- nix
         "nixpkgs-fmt",
-        -- "nixfmt",
-        -- "nil",
       })
     end,
   },
@@ -99,127 +90,17 @@ return {
           },
         },
         html = {
-          filetype = { "html", "templ" },
+          filetypes = { "html", "templ" },
         },
         tailwindcss = {
-          filetype = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "templ" },
+          filetypes = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "templ" },
           init_options = { userLanguages = { templ = "html" } },
         },
-        ts_ls = {
-          enabled = false,
-        },
-        vtsls = {
-          -- explicitly add default filetypes, so that we can extend
-          -- them in related extras
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "javascript.jsx",
-            "typescript",
-            "typescriptreact",
-            "typescript.tsx",
-          },
-          settings = {
-            complete_function_calls = true,
-            vtsls = {
-              enableMoveToFileCodeAction = true,
-              autoUseWorkspaceTsdk = true,
-              experimental = {
-                maxInlayHintLength = 30,
-                completion = {
-                  enableServerSideFuzzyMatch = true,
-                },
-              },
-            },
-            typescript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              suggest = {
-                completeFunctionCalls = true,
-              },
-              inlayHints = {
-                enumMemberValues = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                parameterNames = { enabled = "literals" },
-                parameterTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                variableTypes = { enabled = false },
-              },
-            },
-          },
-          keys = {
-            {
-              "gD",
-              function()
-                local params = vim.lsp.util.make_position_params()
-                LazyVim.lsp.execute({
-                  command = "typescript.goToSourceDefinition",
-                  arguments = { params.textDocument.uri, params.position },
-                  open = true,
-                })
-              end,
-              desc = "Goto Source Definition",
-            },
-            {
-              "gR",
-              function()
-                LazyVim.lsp.execute({
-                  command = "typescript.findAllFileReferences",
-                  arguments = { vim.uri_from_bufnr(0) },
-                  open = true,
-                })
-              end,
-              desc = "File References",
-            },
-            {
-              "<leader>co",
-              LazyVim.lsp.action["source.organizeImports"],
-              desc = "Organize Imports",
-            },
-            {
-              "<leader>cM",
-              LazyVim.lsp.action["source.addMissingImports.ts"],
-              desc = "Add missing imports",
-            },
-            {
-              "<leader>cu",
-              LazyVim.lsp.action["source.removeUnused.ts"],
-              desc = "Remove unused imports",
-            },
-            {
-              "<leader>cD",
-              LazyVim.lsp.action["source.fixAll.ts"],
-              desc = "Fix all diagnostics",
-            },
-            {
-              "<leader>cV",
-              function()
-                LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
-              end,
-              desc = "Select TS workspace version",
-            },
-          },
-        },
-        jdtls = {},
-        -- nil_ls = {},
       },
       setup = {
-        gopls = function(_, opts) end,
-        templ = function(_, opts) end,
-        html_lsp = function(_, opts) end,
-        htmx_lsp = function(_, opts) end,
-        --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
-        --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
-        tsserver = function()
-          -- disable tsserver
-          return true
-        end,
-        ts_ls = function()
-          -- disable tsserver
-          return true
-        end,
-        vtsls = function(_, opts) end,
+        -- jdtls handled by lang.java extra (nvim-jdtls plugin)
         jdtls = function()
-          return true -- avoid duplicate servers
+          return true
         end,
       },
     },
